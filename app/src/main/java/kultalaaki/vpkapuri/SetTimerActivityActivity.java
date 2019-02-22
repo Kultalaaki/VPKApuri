79e4c66c170c363b5d935fe55b56600756b6e0da
Kultala Aki Juhani <kultalaaki@gmail.com>
2019-02-23 00:34:26 +0200

    Timer UI almost ready.

---
+++ app/src/main/java/kultalaaki/vpkapuri/SetTimerActivityActivity.java
@@ -0,0 +1,13 @@
+package kultalaaki.vpkapuri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetTimerActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_timer_activity);
+    }
+}
--- app/src/main/java/kultalaaki/vpkapuri/TimerActivity.java
+++ app/src/main/java/kultalaaki/vpkapuri/TimerActivity.java
@@ -1,13 +1,50 @@
 package kultalaaki.vpkapuri;
 
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TimerActivity extends AppCompatActivity {
+public class TimerActivity extends AppCompatActivity
+        implements SetTimerFragment.OnFragmentInteractionListener {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        fab = findViewById(R.id.floatingActionButton);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSetTimer();
+            }
+        });
+    }
+
+    void openSetTimer() {
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        SetTimerFragment setTimerFragment = new SetTimerFragment();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right);
+        fragmentTransaction.add(R.id.showSetTimer, setTimerFragment, "setTimerFragment");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
