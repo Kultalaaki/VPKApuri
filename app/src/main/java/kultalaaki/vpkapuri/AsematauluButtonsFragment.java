8382bf5e8d5456e4cdf667dd7aad3af35432983a
Kultala Aki Juhani <kultalaaki@gmail.com>
2019-06-17 00:22:58 +0300

    AsematauluButtonsFragment added.

---
+++ app/src/main/java/kultalaaki/vpkapuri/AsematauluButtonsFragment.java
@@ -0,0 +1,175 @@
+package kultalaaki.vpkapuri;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AsematauluButtonsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link AsematauluButtonsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AsematauluButtonsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private CardView osoiteKortti, vahvuudetKortti, kameraKortti;
+
+    private String osoiteFromDB;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public AsematauluButtonsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AsematauluButtonsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AsematauluButtonsFragment newInstance(String param1, String param2) {
+        AsematauluButtonsFragment fragment = new AsematauluButtonsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_asemataulu_buttons, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        osoiteKortti = view.findViewById(R.id.addressCard);
+        kameraKortti = view.findViewById(R.id.cameraCard);
+        vahvuudetKortti = view.findViewById(R.id.responderCard);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        FireAlarmViewModel mViewModel = ViewModelProviders.of(this).get(FireAlarmViewModel.class);
+
+        try {
+            FireAlarm fireAlarm = mViewModel.lastEntry();
+            osoiteFromDB = fireAlarm.getOsoite();
+        } catch (Exception e) {
+            // Empty database
+            osoiteFromDB = "";
+        }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        setOnClickListeners();
+    }
+
+    private void setOnClickListeners() {
+        osoiteKortti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + osoiteFromDB);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                //mapIntent.setPackage("com.google.android.apps.maps");
+                Context context = getActivity();
+                if(context != null) {
+                    if (mapIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                        startActivity(mapIntent);
+                    }
+                }
+            }
+        });
+
+        kameraKortti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.openCamera();
+            }
+        });
+
+        vahvuudetKortti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.loadResponderFragmentPhone();
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void openCamera();
+        void loadResponderFragmentPhone();
+    }
+}
--- app/src/main/java/kultalaaki/vpkapuri/HalytysActivity.java
+++ app/src/main/java/kultalaaki/vpkapuri/HalytysActivity.java
@@ -4,18 +4,35 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class HalytysActivity extends AppCompatActivity
-        implements HalytysButtonsFragment.Listener {
+        implements HalytysButtonsFragment.Listener, AsematauluButtonsFragment.OnFragmentInteractionListener {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     boolean koneluku, autoAukaisu, asemataulu;
-    String action, type;
+    String action, type, currentPhotoPath;
     SharedPreferences preferences;
 
     @Override
@@ -23,7 +40,7 @@ public class HalytysActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         asemataulu = preferences.getBoolean("asemataulu", false);
-        if(!asemataulu) {
+        if (!asemataulu) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
         setContentView(R.layout.activity_halytys);
@@ -37,7 +54,7 @@ public class HalytysActivity extends AppCompatActivity
     public void hiljenna() {
         HalytysFragment halytysFragment = (HalytysFragment)
                 getSupportFragmentManager().findFragmentByTag("halytysFragment");
-        if(halytysFragment != null) {
+        if (halytysFragment != null) {
             halytysFragment.lopetaPuhe();
         }
     }
@@ -46,7 +63,7 @@ public class HalytysActivity extends AppCompatActivity
         HalytysFragment halytysFragment = (HalytysFragment)
                 getSupportFragmentManager().findFragmentByTag("halytysFragment");
         //Log.i("HalytysActivity", halytysFragment.toString());
-        if(halytysFragment != null) {
+        if (halytysFragment != null) {
             halytysFragment.txtToSpeech();
         }
     }
@@ -64,9 +81,12 @@ public class HalytysActivity extends AppCompatActivity
     protected void onStart() {
         super.onStart();
         loadhalytysFragment();
-        if(asemataulu) {
+        if (asemataulu) {
             // TODO: Asemataulu käytössä
-            loadResponderFragment();
+            loadAsematatuluButtons();
+            if (findViewById(R.id.responder_view) != null) {
+                loadResponderFragment();
+            }
         } else {
             loadhalytysButtonsFragment();
         }
@@ -74,13 +94,26 @@ public class HalytysActivity extends AppCompatActivity
         getParameters(action, type);
     }
 
+    void loadAsematatuluButtons() {
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        AsematauluButtonsFragment asematauluButtonsFragment = new AsematauluButtonsFragment();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.HalytysAlaosa, asematauluButtonsFragment, "asematauluButtonsFragment").commit();
+    }
+
     public void loadResponderFragment() {
-        if(findViewById(R.id.responder_view) != null) {
-            FragmentManager fragmentManager = this.getSupportFragmentManager();
-            ResponderFragment responderFragment = new ResponderFragment();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.responder_view, responderFragment, "ResponderFragment").commit();
-        }
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        ResponderFragment responderFragment = new ResponderFragment();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.responder_view, responderFragment, "ResponderFragment").commit();
+    }
+
+    public void loadResponderFragmentPhone() {
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        ResponderFragment responderFragment = new ResponderFragment();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.HalytysYlaosa, responderFragment, "ResponderFragment").commit();
     }
 
     public void loadhalytysFragment() {
@@ -105,12 +138,12 @@ public class HalytysActivity extends AppCompatActivity
                 HalytysFragment halytysFragment = (HalytysFragment)
                         getSupportFragmentManager().findFragmentByTag("halytysFragment");
                 //Log.i("HalytysActivity", halytysFragment.toString());
-                if(halytysFragment != null) {
+                if (halytysFragment != null) {
                     halytysFragment.txtToSpeech();
                 }
                 HalytysButtonsFragment halytysButtonsFragment = (HalytysButtonsFragment)
                         getSupportFragmentManager().findFragmentByTag("halytysButtonsFragment");
-                if(halytysButtonsFragment != null) {
+                if (halytysButtonsFragment != null) {
                     halytysButtonsFragment.setTextHiljennaPuhe();
                 }
             }
@@ -125,7 +158,7 @@ public class HalytysActivity extends AppCompatActivity
             public void run() {
                 HalytysButtonsFragment halytysButtonsFragment = (HalytysButtonsFragment)
                         getSupportFragmentManager().findFragmentByTag("halytysButtonsFragment");
-                if(halytysButtonsFragment != null) {
+                if (halytysButtonsFragment != null) {
                     halytysButtonsFragment.autoAukaisu();
                 }
             }
@@ -137,17 +170,85 @@ public class HalytysActivity extends AppCompatActivity
     @SuppressLint("ApplySharedPref")
     public void getParameters(String action, String type) {
 
-        if (Intent.ACTION_SEND.equals(action)&& type != null) {
-            if ("text/plain".equals(type)){
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
                 Intent stopAlarm = new Intent(HalytysActivity.this, IsItAlarmService.class);
                 HalytysActivity.this.stopService(stopAlarm);
-                if(koneluku && !autoAukaisu) {
+                if (koneluku && !autoAukaisu) {
                     waitForFragment();
                 }
-            } else if("automaattinen".equals(type)) {
+            } else if ("automaattinen".equals(type)) {
                 preferences.edit().putBoolean("showHiljenna", true).commit();
                 waitForButtonsFragment();
             }
+        }
+    }
+
+    /**
+     * AsematauluButtonsFragment methods below this
+     *
+     * <--Methods to taker picture and add it to gallery-->
+     * openCamera
+     * createImageFile
+     * galleryAddPic
+     * onActivityResult
+     */
+    public void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("Camera", "Error occurred while creating the File");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "kultalaaki.vpkapuri",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("dd.MM.yyyy_H:mm:ss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        //File storageApuri = getExternalFilesDir("VPK Apuri");
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //setPic();
+            galleryAddPic();
         }
     }
 }
