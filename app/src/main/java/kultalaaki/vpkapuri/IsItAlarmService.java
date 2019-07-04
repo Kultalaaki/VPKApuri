bc86ce004d8855e9d05048450020f7d77b9228c9
Kultala Aki Juhani <kultalaaki@gmail.com>
2019-07-04 16:12:57 +0300

    Build 3.5. Final tests and release if everything is OuKei.

--- app/src/main/java/kultalaaki/vpkapuri/IsItAlarmService.java
+++ app/src/main/java/kultalaaki/vpkapuri/IsItAlarmService.java
@@ -42,7 +42,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 public class IsItAlarmService extends Service implements MediaPlayer.OnPreparedListener {
