1a0322cf475f35dfc1f618c4267c5629c2daefd6
Kultala Aki <kultalaaki@gmail.com>
2021-08-02 00:37:10 +0300

    Alarm.class added. Contains all info that alarm has. Not finished.

---
+++ app/src/main/java/kultalaaki/vpkapuri/alarm/Alarm.java
@@ -0,0 +1,16 @@
+/*
+ * Created by Kultala Aki on 8/2/21, 12:33 AM
+ * Copyright (c) 2021. All rights reserved.
+ * Last modified 8/2/21, 12:33 AM
+ */
+
+package kultalaaki.vpkapuri.alarm;
+
+public class Alarm {
+
+    private String address;
+    private String alarmID;
+    private String alarmTextField;
+    private boolean isAlarm;
+    private String sender;
+}
